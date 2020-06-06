67fabdcad9799c4610570ce800bb598539c9fc03
ademarCardoso <ac@storyblok.com>
2020-06-06 10:17:53 -0300

    create routes

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,3 +1,13 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+const Detail = () => {
+  return (
+    <View >
+    </View>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -71,3 +81,5 @@ const styles = StyleSheet.create({
     fontFamily: 'Roboto_500Medium',
   },
 });
+
+export default Detail
